91a05d74eed490c305a52dc1c771a87b838b9c2e
Joakim Andersson <joakim@itandersson.se>
2015-08-26 11:37:53 +0200

    Flyttar javascript till fil.

--- kalorinix_codeigniter/application/views/welcome/calorie_counter.php
+++ kalorinix_codeigniter/application/views/welcome/calorie_counter.php
@@ -1,5 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<script>
 
+		var path = "<?php echo site_url('welcome/find_food'); ?>";
+		var counter = new calorieCounter( path );
+
+</script>
 <h2>Välkommen till KaloriNix!</h2>
 
 <!-- Body -->
@@ -11,15 +16,12 @@
 <div class="row">
   <div class="col-md-5">
     <div class="input-group">
-      <input type="search" id="searchTxt" onkeyup="updateList()" class="form-control" data-toggle="collapse" data-target="#dataWell" aria-expanded="false" aria-controls="collapseExample">
+      <input type="search" id="searchTxt" onkeyup="counter.updateList();" class="form-control" placeholder="Sök vara..." data-toggle="collapse" data-target="#dataWell" aria-expanded="false" aria-controls="collapseExample">
       <div class="input-group-btn">
         <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Funktioner <span class="caret"></span></button>
         <ul class="dropdown-menu dropdown-menu-right" role="menu">
-          <li><a href="<?php echo site_url('welcome/create_food'); ?>">Skapa ny matvara</a></li>
+          <li><a href="<?php echo site_url('welcome/create_food'); ?>">Skapa vara</a></li>
           <li><a href="#">Lägg till motion</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Separated link</a></li>
         </ul>
       </div>
     </div>
@@ -27,59 +29,9 @@
          <div class="well">
             <table id="dataTable">
                <tr id="foodText">
-                  <td>Skriv för att söka efter matvaror.</td>
+                  <td>Skriv för att söka efter varor.</td>
                </tr>
             </table>
-
-
-<script>
-$(document).ready(function(){
-	var timeoutID = false;
-
-	/**
-	 * AJAX med REST, en sekunds fördröjning, hanterar food items från datakällan.
-	 * @link http://www.w3schools.com/jsref/met_win_cleartimeout.asp
-	 */
-	updateList = function() {
-		if(timeoutID){window.clearTimeout(timeoutID);}
-		timeoutID = setTimeout(function(){
-			   $("#foodText").hide();
-			   var param = "/" + document.getElementById("searchTxt").value;
-
-			   //AJAX get json
-			   $.getJSON("<?php echo site_url('welcome/find_food'); ?>" + param)
-				    .done(function( jsonData ) {
-					    var count = 0;
-				    	$.each(jsonData, function(index) {
-				    		var $row = '';
-				            
-				    		$row += '<tr><td id=\"item' + (++count) + '\"><span>';
-				    		$row += jsonData[index].food_item;
-				    		$row += '</span></td></tr>';
-
-				    		$( "#dataTable" ).append( $row );
-
-				    		//Event click handler
-				    		$( "#item" + (count) + "" ).on( "click", {
-					    		id_food: jsonData[index].id_food
-					    		}, sendToTable );
-				        });
-				    });
-			   
-			},1000); //1000 milli sekund.
-    };
-
-    /**
-	 * Event handler, Skickar data till tabellen.
-	 */
-    sendToTable = function( event ) {
-		  alert( "Hello " + event.data.id_food );
-		}
-});
-</script>
-
-
-            
          </div>
     </div>
   </div>
--- kalorinix_codeigniter/js/main.js
+++ kalorinix_codeigniter/js/main.js
@@ -1,3 +1,99 @@
+"use strict";
+
+/**
+ * Hanterar hämtning av data från databasen och överlämning till tabellen.
+ * @link http://www.w3schools.com/jsref/met_win_cleartimeout.asp
+ */
+function calorieCounter(p_path){
+	this.path = p_path;
+	this.timeoutID  = false;
+	
+	//AJAX med REST, en sekunds fördröjning, hanterar food items från datakällan.
+	this.updateList = function() {
+		if(this.timeoutID){window.clearTimeout(this.timeoutID);}
+		this.timeoutID = setTimeout(function(){
+			   $("#foodText").hide();
+			   var param = "/" + document.getElementById("searchTxt").value;
+
+			   //AJAX get json
+			   $.getJSON(this.path + param)
+				    .done(function( jsonData ) {
+				    	var count = 0;
+					    $( "#dataTable" ).empty();
+				    	$.each(jsonData, function(index) {
+				    		var row = '';
+				            
+				    		row += '<tr><td id=\"item' + (++count) + '\"><span>';
+				    		row += jsonData[index].food_item;
+				    		row += '</span></td></tr>';
+
+				    		$( "#dataTable" ).append( row );
+
+				    		//Event click handler
+				    		$( "#item" + (count) + "" ).on( "click", {
+				    			id_food: jsonData[index].id_food,
+				    			food_item: jsonData[index].food_item,
+					    		st: jsonData[index].st,
+					    		l: jsonData[index].l, 
+					    		dl: jsonData[index].dl, 
+					    		msk: jsonData[index].msk, 
+					    		tsk: jsonData[index].tsk, 
+					    		krm: jsonData[index].krm
+					    		}, sendToTable );
+				        });
+				    });
+			   
+			},1000); //1000 milli sekund.
+		};
+		
+		//Event handler som initierar och visar modalen send_to_table, som skickar data till tabellen.
+	    var sendToTable = function( event ) {
+
+			  $("#send_to_table_item").text( event.data.food_item + ':' );
+
+			  $( "#selectOptions" ).empty();
+			  
+			  //Standard enhet.
+			  $("#selectTableOptions").append( "<option value=\"g\">Gram (g)</option>" );
+			  
+			  if(event.data.st > 0)//Lägg till om extra enheter är över 0.
+			  {
+				  $("#selectTableOptions").append( "<option value=\"st\">Stycken (st):</option>" );
+			  }
+			  if(event.data.l > 0)//Lägg till om extra enheter är över 0.
+			  {
+				  $("#selectTableOptions").append( "<option value=\"l\">Liter (l)</option>" );
+			  }
+			  if(event.data.dl > 0)//Lägg till om extra enheter är över 0.
+			  {
+				  $("#selectTableOptions").append( "<option value=\"dl\">Deciliter (dl):</option>" );
+			  }
+			  if(event.data.msk > 0)//Lägg till om extra enheter är över 0.
+			  {
+				  $("#selectTableOptions").append( "<option value=\"msk\">Matsked (msk):</option>" );
+			  }
+			  if(event.data.tsk > 0)//Lägg till om extra enheter är över 0.
+			  {
+				  $("#selectTableOptions").append( "<option value=\"tsk\">Tesked (tsk):</option>" );
+			  }
+			  if(event.data.krm > 0)//Lägg till om extra enheter är över 0.
+			  {
+				  $("#selectTableOptions").append( "<option value=\"krm\">Kryddmått (krm):</option>" );
+			  }
+			  
+			  //Visa modal.
+			  $('#send_to_table').modal('show');
+			};
+    }
+	
+	
+    
+    
+    
+
+
+
+
 /**
  * Google jsapi Visar diagram.
  * @Link https://www.google.com/jsapi
